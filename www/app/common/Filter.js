1d6ddff7591b99327e809d43eb2bc5bc1f8063c3
ubuntu <ben.ali.ali.86@gmail.com>
2016-05-20 23:12:58 +0200

    format_date_consultation

---
+++ www/app/common/Filter.js
@@ -0,0 +1,30 @@
+appContext.filter("customDate", function() {
+
+    //Defining the filter function
+     return function(input) {
+
+             var result = "";
+             input = input || "";
+
+             try {
+
+ 	    		    var array1 = input.split("-"); //array1[0] : annéé //array1[1] : mois
+ 		          var array2 = array1[2].split(" "); //array2[0] jour
+   		        var array3 = array2[1].split(":");
+   		        if (array1[1].length == 1 )
+   		        	array1[1] = "0"+array1[1];
+   		        if (array1[0].length == 1 )
+   		        	array1[0] = "0"+array1[0];
+   		        if (array3[0].length == 1 )
+   		        	array3[0] = "0"+array3[0];
+   		        if(array2[2] =="PM" && array3[0] != "12" )
+   		        	array3[0] = parseInt(parseInt(array3[0]) + 12);
+   		        result = array2[0] +"/"+ array1[1] +"/"+array1[0]+" à "+array3[0]+":"+array3[1];
+   		        return  result;
+
+ 			} catch (e) {
+// 				console.error(e);
+ 				return input;
+ 			}
+     };
+});
